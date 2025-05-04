967522044d835ae7275dd9197aa003582eedebac
drewman301 <abraddock8104@gmail.com>
2025-05-04 18:20:50 -0500

    moved tracker functionality to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,57 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Owo from './Owo'
-import ApiFetcher from './ApiFetcher'
 import ISSTracker from './ISSTracker'
+import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents } from 'react-leaflet'
+import './ISSTracker.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  let url = "https://api.wheretheiss.at/v1/satellites/25544";
+  const [result, setResult] = useState();
+  const [buttonText, setButtonText] = useState("Send Request");
+
+  function sendRequest(event){
+      const request = new Request(url); //construct the HTTP request.
+      fetch(request) //Sends the request, returns a Promise.
+          .then((res) => res.json()) // decode JSON
+          .then((object) => {
+              console.log(object);
+
+              setButtonText("Resend Request")
+              setResult(object);
+          });
+  }
 
   return (
     <>
-      <div hidden>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <div className="card">
-        <button hidden onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <hr />
-        <ISSTracker />
-        <p>
+      <div className="api-fetcher">
+            <h1>Where ISS?</h1>
+            <br />
+            <button onClick={sendRequest}>{buttonText}</button>
+            {result && 
+            <p>The ISS is in {result.visibility} at{' '}
+                <a className="coordslink" target="_blank" href={`https://www.google.com/maps/place/${result.latitude},${result.longitude}`}>
+                    {result.latitude}, {result.longitude}.
+                </a>
+            </p>
+            }
+        </div>
+            <MapContainer style={{ height: '500px', width: '100%' }} className="main-map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {result && (
+                    <Marker position={[result.latitude, result.longitude]}>
+                        <Popup>
+                          A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                )}
+            </MapContainer>
+        <p hidden>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
