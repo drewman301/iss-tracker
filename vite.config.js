73e79be7c87a1d2a4246edcbe719604f9afe69e5
drewman301 <abraddock8104@gmail.com>
2025-05-04 21:29:04 -0500

    Changed base URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/iss-tracker/"
+  base: "https://drewman301.github.io/iss-tracker/"
 })
